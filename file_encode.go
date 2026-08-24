836744898a2383a606dd5a1eaba957de9cb276f2
docRonal <formyselfandother1@gmail.com>
2026-08-24 16:34:43 +0200

    Fix formatting of error messages in file_encode.go

--- file_encode.go
+++ file_encode.go
@@ -13,16 +13,16 @@ keyBytes := []byte(key)
 
 file, err:= os.ReadFile("test.txt")
 	if err!=nil{
-	fmt.Println("Couldnt open file", err)
-	return 
+		fmt.Println("Couldnt open file", err)
+		return 
 	}
 	for i:=0; i<len(file);i++{
-	file[i] = file[i] ^ keyBytes[i % len(keyBytes)]
+		file[i] = file[i] ^ keyBytes[i % len(keyBytes)]
 	}
 	err = os.WriteFile("test.enc", file, 0644)
 	if err!=nil{
-	fmt.Println("Couldnt save file", err)
-	return 
+		fmt.Println("Couldnt save file", err)
+		return 
 	}
 	fmt.Println("All good")
 
