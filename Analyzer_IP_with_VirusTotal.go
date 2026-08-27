4bd6c47aaecd67c18ff5a2149f84bfb39aa980e5
docRonal <formyselfandother1@gmail.com>
2026-08-27 21:46:59 +0200

    Enhance VirusTotal IP analysis with error handling
    Added a new struct for parsing VirusTotal API response and updated the main function to handle API errors and display relevant information.

--- Analyzer_IP_with_VirusTotal.go
+++ Analyzer_IP_with_VirusTotal.go
@@ -4,7 +4,20 @@ import(
 "fmt"
 "io"
 "net/http"
+"encoding/json"
 )
+
+type VTResposnse struct{
+	Data struct{
+		Atributes struct{
+			Owner string `json:"as_owner"`
+			Stats struct{
+				Malicious int `json:"malicious"`
+				Harmless  int `json:"harmless"`
+			} `json:"last_analysis_stats"`
+		} `json:"attributes"`
+	} `json:"data"`
+}
 
 func main() {
 
@@ -27,7 +40,23 @@ req, err:= http.NewRequest("GET", url, nil)
 	defer resp.Body.Close()
 	
 	bodyBytes, _ := io.ReadAll(resp.Body)
-	fmt.Println("The server answer: ", string(bodyBytes))
+	if resp.StatusCode != 200 {
+		fmt.Println ("API error", resp.StatusCode)
+		fmt.Println ("Unsorted data", string(bodyBytes))
+		return 
+	}
+	
+	var result VTResposnse
+	
+	err = json.Unmarshal(bodyBytes, &result)
+	if err!=nil{
+		fmt.Println("Cant pars JSON file", err)
+		return
+	}
+	
+	
+	fmt.Println("The server owner: ", result.Data.Atributes.Owner)
+	fmt.Println("The server save part: ", result.Data.Atributes.Stats.Malicious)
+	fmt.Println("The server danger part ", result.Data.Atributes.Stats.Harmless)
+	
 }
-
-
