cf8f04abba38855b9785290da6c33f9d5576bdcf
docRonal <formyselfandother1@gmail.com>
2026-09-02 16:40:05 +0200

    Update Log_analyzer.go

--- Log_analyzer.go
+++ Log_analyzer.go
@@ -16,13 +16,21 @@ data, err:= os.ReadFile("access.log")
 	}
 lines :=strings.Split(string(data), "\n")
 sqlPattern := regexp.MustCompile(`(?i)union.*select`)
+XSSPatern := regexp.MustCompile(`(?i)<script`)
+PathPatern := regexp.MustCompile(`\.\./`)
 
 	for _, line :=range lines {
 		if sqlPattern.MatchString(line){	
 				fmt.Println("ALERT: SQL Injection detected:", line)
-			
+		}
+		if XSSPatern.MatchString(line){
+			fmt.Println("XSS atack detected", line)
+		}
+		if PathPatern.MatchString(line){
+			fmt.Println("Path Traversal detected", line)
 		}
 
 	}
 	
 }
+
